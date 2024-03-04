91838cabcdf995dd74f913172f83c5edc9c0f331
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-04 22:41:46 +0800

    initial type definitions

---
+++ src/schema.ts
@@ -0,0 +1,20 @@
+export const typeDefs = `#graphql
+
+  type Address {
+    street: String!
+    city: String!
+  }
+
+  type Person {
+    name: String!
+    phone: String
+    address: Address!
+    id: ID!
+  }
+
+  type Query {
+    personCount: Int!
+    allPersons: [Person!]!
+    findPerson(name: String!): Person
+  }
+`;
