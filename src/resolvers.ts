603c9c69c67873487a1bb80e1f93e493e770f53e
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-05 08:54:38 +0800

    add Mutation resolver for addPerson

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,4 +1,6 @@
 import db from './_db';
+import { v1 as uuid } from 'uuid';
+import { Person } from './types';
 
 let persons = db.persons;
 
@@ -6,15 +8,25 @@ export const resolvers = {
   Query: {
     personCount: () => persons.length,
     allPersons: () => persons,
-    findPerson: (_root: unknown, args: { name: string; }) => 
-      persons.find(p => p.name === args.name)
+    findPerson: (_root: unknown, args: { name: string }) =>
+      persons.find((p) => p.name === args.name),
   },
   Person: {
-    address: (root: { street: string; city: string; }) => {
+    address: (root: { street: string; city: string }) => {
       return {
         street: root.street,
-        city: root.city
+        city: root.city,
       };
-    }
+    },
+  },
+  Mutation: {
+    addPerson: (_root: unknown, args: Person ) => {
+      const newPerson: Person = {
+        ...args,
+        // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
+        id: uuid(),
+      };
+      persons = persons.concat(newPerson);
+    },
   },
 };
