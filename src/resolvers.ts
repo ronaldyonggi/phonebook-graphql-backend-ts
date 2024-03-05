441507fedc5154b02c182187102503c8b67abdbd
Ronald Yonggi <ronaldyonggi@gmail.com>
2024-03-05 08:54:55 +0800

    addPerson error handler for duplicate name

--- src/resolvers.ts
+++ src/resolvers.ts
@@ -1,6 +1,7 @@
 import db from './_db';
 import { v1 as uuid } from 'uuid';
 import { Person } from './types';
+import { GraphQLError } from 'graphql';
 
 let persons = db.persons;
 
@@ -21,6 +22,15 @@ export const resolvers = {
   },
   Mutation: {
     addPerson: (_root: unknown, args: Person ) => {
+      // If the name already exist, throw error
+      if (persons.find(p => p.name === args.name)) {
+        throw new GraphQLError('Name must be unique!', {
+          extensions: {
+            code: 'BAD_USER_INPUT',
+            invalidArgs: args.name
+          }
+        });
+      }
       const newPerson: Person = {
         ...args,
         // eslint-disable-next-line @typescript-eslint/no-unsafe-assignment, @typescript-eslint/no-unsafe-call
